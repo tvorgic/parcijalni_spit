cb1135f69aeca81dbf28cd2e14ec33309c9c6152
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2024-02-06 23:51:17 +0100

    parcijalni zadatak

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,59 @@
+﻿using Ispit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Console.WriteLine("Unesite ime, prezime, datum rođenja(DD/MM/YYY) i prosjek prvog učenika");
+            Ucenik ucenik1 = new Ucenik();
+            ucenik1.ime = Console.ReadLine();
+            ucenik1.prezime = Console.ReadLine();   
+            ucenik1.datumRodjenja = DateTime.Parse(Console.ReadLine()); 
+            ucenik1.prosjek = ucenik1.Prosjek(4.5);
+            //Console.WriteLine(ucenik1.Starost(05/05/1985,2024));
+            Console.WriteLine($"Student: {ucenik1}\n");
+            Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
+
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Unesite ime, prezime, datum rođenja(DD/MM/YYY) i prosjek drugog učenika");
+
+            Ucenik ucenik2 = new Ucenik();
+            ucenik2.ime = Console.ReadLine();
+            ucenik2.prezime = Console.ReadLine();
+            ucenik2.datumRodjenja = DateTime.Parse(Console.ReadLine());
+            ucenik2.prosjek = ucenik2.Prosjek(3.5);
+
+
+            Console.WriteLine("Unesite ime, prezime, datum rođenja(DD/MM/YYY) i prosjek drugog učenika");
+
+            Ucenik ucenik3 = new Ucenik();
+            ucenik3.ime = Console.ReadLine();
+            ucenik3.prezime = Console.ReadLine();
+            ucenik3.datumRodjenja = DateTime.Parse(Console.ReadLine());
+            ucenik3.prosjek = ucenik3.Prosjek(2.5);
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        public string ime;
+        public string prezime;
+        public DateTime datumRodjenja;
+        public double prosjek;
+
+
+
+        public   void Starost(DateTime datumRodjenja, DateTime tekuciDatum)
+        {
+            int starost = tekuciDatum.Year - datumRodjenja.Year ;
+
+            Console.WriteLine(starost);
+
+
+        }
+
+        public  double Prosjek(double prosjek)
+        {
+            if(prosjek == 1 || prosjek <= 1.49)
+            {
+                Console.WriteLine("nedovoljan");
+            }
+            else if (prosjek == 1.50 ||  prosjek <= 2.49)
+            {
+                Console.WriteLine("dovoljan");
+            }
+            else if(prosjek == 2.50 ||  prosjek <= 3.49)
+            {
+                 Console.WriteLine("dobar");
+            }
+            else if(prosjek == 3.50 || prosjek <= 4.49) 
+            {
+                Console.WriteLine("vrlo dobar");
+             }
+            else if(prosjek == 4.50 || prosjek <= 5)
+            {
+                Console.WriteLine("odličan");
+            }
+        }
+    }
+
+    
+
+    
+
+}
